a11220066f449cfca1cc161d1463dbc9d121a350
Guilherme Miranda <saguilherme16@gmail.com>
2025-07-21 23:12:41 -0300

    FEAT: add OrderItemRepository interface for order item data access

---
+++ src/main/java/com/example/course/repositories/OrderItemRepository.java
@@ -0,0 +1,8 @@
+package com.example.course.repositories;
+
+import com.example.course.entities.OrderItem;
+import com.example.course.entities.pk.OrderItemPK;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, OrderItemPK> {
+}
